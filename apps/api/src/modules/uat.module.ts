84450e966fc25632d843d087123436b9aabdf66a
CFI21 <cfikabul@gmail.com>
2026-09-13 15:45:13 +0200

    Fix UAT module auth import

--- apps/api/src/modules/uat.module.ts
+++ apps/api/src/modules/uat.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { UatController } from './uat.controller';
 import { UatService } from './uat.service';
 
