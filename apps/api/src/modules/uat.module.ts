baca6154e6b74d863ad037ad015b263ecba0f1fc
CFI21 <cfikabul@gmail.com>
2026-09-13 15:53:02 +0200

    Wire one-time UAT bootstrap into staging module

--- apps/api/src/modules/uat.module.ts
+++ apps/api/src/modules/uat.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UatController } from './uat.controller';
 import { UatService } from './uat.service';
+import { UatBootstrapService } from './uat.bootstrap';
 
 @Module({
   imports: [AuthModule],
   controllers: [UatController],
-  providers: [UatService],
+  providers: [UatService, UatBootstrapService],
 })
 export class UatModule {}
