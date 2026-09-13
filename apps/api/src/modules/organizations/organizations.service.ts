f201e218865150d8daa2b665061f4cc941cb7bad
CFI21 <cfikabul@gmail.com>
2026-09-13 20:47:28 +0200

    Scope ANCLINE organization creation to internal users

--- apps/api/src/modules/organizations/organizations.service.ts
+++ apps/api/src/modules/organizations/organizations.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { ScopeService } from '../auth/scope.service';
+import { ScopeUser } from '../auth/scope';
+
 @Injectable()
 export class OrganizationsService {
-  constructor(private prisma:PrismaService){}
+  constructor(private prisma:PrismaService,private scope:ScopeService){}
   list(){ return this.prisma.organization.findMany({orderBy:{name:'asc'}}); }
   get(id:string){ return this.prisma.organization.findUnique({where:{id}}); }
-  create(body:any){ return this.prisma.organization.create({data:body}); }
+  create(body:any,user:ScopeUser){ this.scope.assertInternal(user); return this.prisma.organization.create({data:body}); }
 }
