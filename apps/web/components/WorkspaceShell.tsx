e7c80c26357fd8699a09d8b649699600a452f84d
CFI21 <cfikabul@gmail.com>
2026-09-13 19:21:04 +0200

    Add exceptions board to ANCLINE navigation

--- apps/web/components/WorkspaceShell.tsx
+++ apps/web/components/WorkspaceShell.tsx
@@ -6,6 +6,7 @@ import {signOut} from '../lib/api';
 const links=[
   ['/', 'Control Tower'],
   ['/bookings','Bookings'],
+  ['/exceptions','Exceptions / Action Board'],
   ['/routing','Routing / Voyage Plan'],
   ['/tracking','Shipment Tracking'],
   ['/organizations','Organizations'],
