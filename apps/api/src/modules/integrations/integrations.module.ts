454ea879df8d7832afd16336004de75dd1a0eed2
CFI21 <cfikabul@gmail.com>
2026-09-14 14:36:16 +0200

    Register integrations module

---
+++ apps/api/src/modules/integrations/integrations.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+import { AuditModule } from '../audit/audit.module';
+import { IntegrationsController } from './integrations.controller';
+import { IntegrationsService } from './integrations.service';
+
+@Module({imports:[PrismaModule,AuthModule,AuditModule],controllers:[IntegrationsController],providers:[IntegrationsService]})
+export class IntegrationsModule {}
