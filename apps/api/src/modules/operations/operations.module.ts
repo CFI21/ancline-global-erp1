c101def6c13ea8e12905ac9e2b06cde20ce85a46
CFI21 <cfikabul@gmail.com>
2026-09-14 14:10:38 +0200

    Register integration processing module

--- apps/api/src/modules/operations/operations.module.ts
+++ apps/api/src/modules/operations/operations.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { OperationsController } from './operations.controller';
 import { OperationsService } from './operations.service';
 import { AlertEngineService } from './alert-engine.service';
+import { IntegrationsController } from './integrations.controller';
+import { IntegrationsService } from './integrations.service';
 
-@Module({controllers:[OperationsController],providers:[OperationsService,AlertEngineService]})
+@Module({controllers:[OperationsController,IntegrationsController],providers:[OperationsService,AlertEngineService,IntegrationsService]})
 export class OperationsModule {}
