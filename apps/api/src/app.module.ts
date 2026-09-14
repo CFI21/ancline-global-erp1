aa0c623208326b3bc0e6b7ccaa21abe0f0a53715
CFI21 <cfikabul@gmail.com>
2026-09-14 07:55:37 +0200

    Add files via upload

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuditModule } from './modules/audit/audit.module';
 import { ObservabilityModule } from './modules/observability/observability.module';
@@ -15,21 +15,52 @@ import { StorageModule } from './modules/storage/storage.module';
 import { PortalModule } from './modules/portal/portal.module';
 import { RequestLoggingMiddleware } from './request-logging.middleware';
 import { DiagnosticsModule } from './modules/diagnostics/diagnostics.module';
-import { UatModule } from './modules/uat.module';
 import { OperationsModule } from './modules/operations/operations.module';
+import { FleetModule } from './modules/fleet/fleet.module';
+import { SpecialCargoModule } from './modules/special-cargo/special-cargo.module';
+import { DocumentOpsModule } from './modules/document-ops/document-ops.module';
+import { LandsideModule } from './modules/landside/landside.module';
+import { CommercialModule } from './modules/commercial/commercial.module';
+import { GovernanceModule } from './modules/governance/governance.module';
+import { AutomationModule } from './modules/automation/automation.module';
+import { UatModule } from './modules/uat/uat.module';
 import { TrackingModule } from './modules/tracking/tracking.module';
 import { RoutingModule } from './modules/routing/routing.module';
-import { ContainerMovementsModule } from './modules/container-movements/container-movements.module';
 
 @Module({
-  imports:[
-    PrismaModule,AuditModule,ObservabilityModule,AuthModule,HealthModule,BookingsModule,
-    OrganizationsModule,RatesModule,DocumentsModule,FinanceModule,TasksModule,
-    ApprovalsModule,StorageModule,PortalModule,DiagnosticsModule,UatModule,OperationsModule,TrackingModule,RoutingModule,ContainerMovementsModule
-  ]
+  imports: [
+    PrismaModule,
+    AuditModule,
+    ObservabilityModule,
+    AuthModule,
+    HealthModule,
+    BookingsModule,
+    OrganizationsModule,
+    RatesModule,
+    DocumentsModule,
+    FinanceModule,
+    TasksModule,
+    ApprovalsModule,
+    StorageModule,
+    PortalModule,
+    DiagnosticsModule,
+    OperationsModule,
+    FleetModule,
+    SpecialCargoModule,
+    DocumentOpsModule,
+    LandsideModule,
+    CommercialModule,
+    GovernanceModule,
+    AutomationModule,
+    UatModule,
+    TrackingModule,
+    RoutingModule,
+  ],
 })
 export class AppModule implements NestModule {
-  configure(consumer:MiddlewareConsumer){
-    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestLoggingMiddleware)
+      .forRoutes({ path: '{*path}', method: RequestMethod.ALL });
   }
 }
