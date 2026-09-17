bff594843684d9f55fbf7cf5834ea599b555242f
CFI21 <cfikabul@gmail.com>
2026-09-17 06:04:55 +0200

    Wire sales CRM module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -21,6 +21,7 @@ import { ProcurementModule } from './modules/procurement/procurement.module';
 import { VendorControlModule } from './modules/vendor-control/vendor-control.module';
 import { CommercialProfitabilityModule } from './modules/commercial-profitability/commercial-profitability.module';
 import { CommercialGuardrailsModule } from './modules/commercial-guardrails/commercial-guardrails.module';
+import { SalesCrmModule } from './modules/sales-crm/sales-crm.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { ApprovalsModule } from './modules/approvals/approvals.module';
 import { StorageModule } from './modules/storage/storage.module';
@@ -39,5 +40,5 @@ import { CustomsModule } from './modules/customs/customs.module';
 import { ShipmentControlModule } from './modules/shipment-control/shipment-control.module';
 import { CommercialModule } from './modules/commercial/commercial.module';
 
-@Module({imports:[PrismaModule,AuditModule,ObservabilityModule,AuthModule,HealthModule,BookingsModule,OrganizationsModule,RatesModule,DocumentsModule,FinanceModule,AccountingModule,CreditControlModule,GeneralLedgerModule,FinanceReportingModule,GroupFinanceModule,StatutoryFinanceModule,TreasuryModule,MonthEndModule,ProcurementModule,VendorControlModule,CommercialProfitabilityModule,CommercialGuardrailsModule,TasksModule,ApprovalsModule,StorageModule,PortalModule,DiagnosticsModule,UatModule,OperationsModule,TrackingModule,RoutingModule,ContainerMovementsModule,SchedulesModule,IntegrationsModule,TransportModule,CustomsModule,ShipmentControlModule,CommercialModule]})
+@Module({imports:[PrismaModule,AuditModule,ObservabilityModule,AuthModule,HealthModule,BookingsModule,OrganizationsModule,RatesModule,DocumentsModule,FinanceModule,AccountingModule,CreditControlModule,GeneralLedgerModule,FinanceReportingModule,GroupFinanceModule,StatutoryFinanceModule,TreasuryModule,MonthEndModule,ProcurementModule,VendorControlModule,CommercialProfitabilityModule,CommercialGuardrailsModule,SalesCrmModule,TasksModule,ApprovalsModule,StorageModule,PortalModule,DiagnosticsModule,UatModule,OperationsModule,TrackingModule,RoutingModule,ContainerMovementsModule,SchedulesModule,IntegrationsModule,TransportModule,CustomsModule,ShipmentControlModule,CommercialModule]})
 export class AppModule implements NestModule {configure(consumer:MiddlewareConsumer){consumer.apply(RequestLoggingMiddleware).forRoutes({path:'{*path}',method:RequestMethod.ALL});}}
