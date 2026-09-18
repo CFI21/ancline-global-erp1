37f9f35961622efe970b43364ede938e1e669381
CFI21 <cfikabul@gmail.com>
2026-09-18 08:53:55 +0200

    Build finance close orchestration control

---
+++ apps/api/src/modules/finance-close-orchestration/finance-close-orchestration.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RevenueCloseModule } from '../revenue-close/revenue-close.module';
+import { FinanceCloseOrchestrationController } from './finance-close-orchestration.controller';
+import { FinanceCloseOrchestrationService } from './finance-close-orchestration.service';
+
+@Module({
+  imports:[RevenueCloseModule],
+  controllers:[FinanceCloseOrchestrationController],
+  providers:[FinanceCloseOrchestrationService]
+})
+export class FinanceCloseOrchestrationModule {}
