7b7ec00bfe9a928630b96b0d58cbaeac89f1b174
CFI21 <cfikabul@gmail.com>
2026-09-19 00:26:38 +0200

    Register UAT startup bootstrap

--- apps/api/src/modules/uat.module.ts
+++ apps/api/src/modules/uat.module.ts
@@ -4,12 +4,13 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { UatController } from './uat.controller';
 import { UatService } from './uat.service';
 import { IntegrationsModule } from './integrations/integrations.module';
+import { UatBootstrapService } from './uat.bootstrap';
 
 const UAT_ENABLED = process.env.UAT_RUNNER_ENABLED === 'true';
 
 @Module({
   imports: UAT_ENABLED ? [AuthModule, PrismaModule, IntegrationsModule] : [],
   controllers: UAT_ENABLED ? [UatController] : [],
-  providers: UAT_ENABLED ? [UatService] : [],
+  providers: UAT_ENABLED ? [UatService, UatBootstrapService] : [],
 })
 export class UatModule {}
