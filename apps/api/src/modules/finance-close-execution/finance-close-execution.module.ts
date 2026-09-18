f8e17e7eb129d9d4be55b77ce153fae69096672a
CFI21 <cfikabul@gmail.com>
2026-09-18 12:41:11 +0200

    Carry finance close bulk onto latest main

---
+++ apps/api/src/modules/finance-close-execution/finance-close-execution.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FinanceCloseOrchestrationModule } from '../finance-close-orchestration/finance-close-orchestration.module';
+import { FinanceCloseExecutionController } from './finance-close-execution.controller';
+import { FinanceCloseExecutionService } from './finance-close-execution.service';
+
+@Module({
+  imports:[FinanceCloseOrchestrationModule],
+  controllers:[FinanceCloseExecutionController],
+  providers:[FinanceCloseExecutionService],
+  exports:[FinanceCloseExecutionService]
+})
+export class FinanceCloseExecutionModule {}
